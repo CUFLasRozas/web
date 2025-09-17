bb88095e4fb85acf4040cb940ff125ac0fbd6a1c
CUFLasRozas <cuflasrozas@hotmail.com>
2025-09-17 13:14:55 +0200

    actualizado scroll-to-top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from "./components/cabecera/cabecera.component";
 import { BarraLateralComponent } from "./components/barra-lateral/barra-lateral/barra-lateral.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent, BarraLateralComponent, FooterComponent],
+  imports: [RouterOutlet, CabeceraComponent, BarraLateralComponent, FooterComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,33 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cuflr-scroll-to-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './scroll-to-top.component.html',
+  styleUrl: './scroll-to-top.component.css'
+})
+export class ScrollToTopComponent implements OnInit  {
+
+   showButton = true;
+  private alturaMinima = 200; // Altura en píxeles para mostrar el botón
+
+  constructor() { }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.showButton = window.scrollY > this.alturaMinima;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  ngOnInit(): void {
+    // Asegúrate de que el botón aparezca o desaparezca al cargar la página
+    this.showButton = window.scrollY > this.alturaMinima;
+  }
+}
